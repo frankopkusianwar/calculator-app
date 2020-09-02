3acfddd826f6e74db1e16fce07b45700866c66ce
frankopkusianwar <okirorfrank3@gmail.com>
2020-09-03 01:49:55 +0300

    fix eslint errors

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 export default function Button(props) {
+  const { name } = props;
   return (
-    <button className="button">
-        {props.name}
+    <button type="submit" className="button">
+      {name}
     </button>
   );
 }
 
 Button.propTypes = {
   name: PropTypes.string,
+};
+
+Button.defaultProps = {
+  name: '0',
 };
