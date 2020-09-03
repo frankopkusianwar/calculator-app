e86b30a84a409613b2b38944950ca4332defc3c5
frankopkusianwar <okirorfrank3@gmail.com>
2020-09-03 13:14:52 +0300

    Add calculate method to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import calculateMethod from '../logic/calculate';
 import Display from './Display';
 import ButtonPannel from './ButtonPanel';
 
 export default function App() {
+  const { calculate } = calculateMethod
   return (
     <div id="App">
       <Display />
