a50cd02e76dda21b048774ecb59d1ea26d4276ba
frankopkusianwar <okirorfrank3@gmail.com>
2020-09-03 13:08:50 +0300

    Updates calculator file

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,9 +1,9 @@
-import operate from './operate';
+import operationsMethod from './operate';
 
 const calculate = (data, name) => {
   const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   const operations = ['+', '-', '/', '*'];
-  // const { operate } = operationsMethod;
+  const { operate } = operationsMethod;
   if (digits.includes(name) && data.next === null) {
     data.next = name;
   } else if (name === '.' && data.operation === null) {
