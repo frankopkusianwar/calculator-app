afbb0a4b244b3e8532a828712578ef8eac2e7aa8
frankopkusianwar <okirorfrank3@gmail.com>
2020-09-03 16:52:36 +0300

    Initialize state in App component

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,13 @@ import ButtonPannel from './ButtonPanel';
 export default class App extends Component {
   constructor() {
     super();
+    this.state = {
+      obj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
   }
   // eslint-disable-next-line no-unused-vars
   render() {
